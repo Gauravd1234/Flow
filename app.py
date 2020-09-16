055ca0d7615f9ab11059e36e6b7612e2d78351bf
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2020-09-16 09:54:20 -0700

    update#2

--- app.py
+++ app.py
@@ -1,19 +1,53 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 
 app = Flask(__name__)
+app.secret_key = "Flow"
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route("/user")
+def user():
+    
+    if "username" in session:
+        username = session["username"]
+        return render_template("user.html", usr = username)   
+
+    else:
+        return redirect(url_for("login"))
+
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        user = request.form["username"]
+        key = request.form["password"]
+        
+        session["username"] = user
+        session["logged_in"] = True
+
+        flash("Successfully signed in", "info")
+        return redirect(url_for("user"))
+
+    else:
+        if "username" in session:
+            return redirect(url_for("user"))
+            
+        return render_template("login.html")
+
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
     return render_template("register.html")
-    
+
+
+@app.route("/logout")
+def logout():
+    flash("Logged out successfully", "info")
+
+    session.clear()
+    return redirect(url_for("login"))
+
 if __name__ == "__main__":
     app.run(debug=True)
