407d93438663d35ef1a3d6d1eee440e25b8a7c33
Gauravd1234 <52632861+Gauravd1234@users.noreply.github.com>
2020-09-16 11:12:47 -0700

    login register

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
 app.secret_key = "Flow"
+
+db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = app.secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+
+class User(db.Model):
+    # The id variable contains the id of every table and item in the database
+    _id = db.Column(db.Integer, primary_key=True)
+
+    # 255 is the largest characteer entry that can be stored in a database
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.email = email
+        self.password = password
+
+
 
 @app.route("/")
 def home():
@@ -12,34 +36,72 @@ def user():
     
     if "username" in session:
         username = session["username"]
-        return render_template("user.html", usr = username)   
+        return render_template("user.html", username = username)   
 
     else:
         return redirect(url_for("login"))
 
+@app.route("/view")
+def view():
+    return render_template("view.html", values = User.query.all())
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
     if request.method == "POST":
         user = request.form["username"]
         key = request.form["password"]
-        
-        session["username"] = user
-        session["logged_in"] = True
 
-        flash("Successfully signed in", "info")
-        return redirect(url_for("user"))
+        if "username" in session:
+            return redirect(url_for("user", username = user))
+
+        
+        
+        
+        if len(User.query.filter_by(username=user).all()) > 0:
+            if (User.query.filter_by(username=user).first().password) == key:
+                flash("Successfully signed in", "info")
+                session["username"] = user
+                session["logged_in"] = True
+                return redirect(url_for("user", username = user))
+
+            else:
+                flash("Credentials invalid", "info")
+                return redirect(url_for("login"))
+
+        else:
+            flash("User not found", "info")
+            return redirect(url_for("login"))
+        
 
     else:
-        if "username" in session:
-            return redirect(url_for("user"))
             
         return render_template("login.html")
 
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        user = request.form["username"]
+        key = request.form["password"]
+        
+        if(len(User.query.filter_by(username=user).all()) > 0 or len(User.query.filter_by(email=email).all()) > 0):
+            flash("User already exists")
+            return redirect(url_for("registration"))
+
+        else:
+            new_user = User(user, key, email)
+            db.session.add(new_user)
+            db.session.commit()
+
+
+            session["username"] = user
+            session["logged_in"] = True
+
+            return redirect(url_for("home"))
+
+    else:
+        return render_template("register.html")
 
 
 @app.route("/logout")
@@ -50,4 +112,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
